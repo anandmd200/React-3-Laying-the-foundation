8f33e2dad01a006660ad3883b4d61d895bf74696
Anand Kumar <anand.kumar@RSK-BSL.COM>
2023-01-07 09:34:31 +0530

    Created header functional component

--- App.js
+++ App.js
@@ -1,40 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./assets/google.png";
 
-const heading = (
-  <h1 className="title" key="myheading">
-    Hello world
-  </h1>
+// logo Search-bar user
+
+const sideLogo = (
+  <span className="title" key="myheading">
+    <img src={logo} alt="Logo" />
+  </span>
 );
 
-/**
- * Component
- * 1. Functional component
- * 2. Class component
- */
-
-const Welcome = () => {
-  return <h1>Welcome to React world</h1>;
-};
-
-/**
- * here heading is a react element and if you see it is like a js variable.
- * inside curley braces you can write any peacie of javaScript
- */
-
-const HeaderComponent = function(){
+const user = (
+  <div>
+    <span>
+      <span className="user-name">Welcome Anand</span>
+      <i className="fa-solid fa-circle-user user"></i>
+    </span>
+  </div>
+);
+const SearchBarComponent = () => {
   return (
-    <div>
-      {heading}                            
-      <Welcome />  
-      {Welcome()}
-      {console.log(1+2)}
-      <h1>Hello from Functional component</h1>
-      <h2>heading 2</h2>
-    </div>
+    <form>
+      <div>
+      <input className="input-Search" type="text" name="name" placeholder="Search"/>
+        <i class="fa-sharp fa-solid fa-magnifying-glass"></i>
+      </div>
+    </form>
   );
 };
 
+const HeaderComponent = function () {
+  return (
+    <div className="header-container">
+      {sideLogo}
+      <SearchBarComponent />
+      {user}
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(<Welcome />);
